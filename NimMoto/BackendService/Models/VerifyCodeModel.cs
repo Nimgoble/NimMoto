13f94598e290b81d06ee7a2e834afd75529928a1
Nimgoble <nimgoble@gmail.com>
2014-12-23 15:21:55 -0700

    More Email Verification
    Since GitHub for Windows didn't pick these changes up the first time...

---
+++ NimMoto/BackendService/Models/VerifyCodeModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.ComponentModel.DataAnnotations;
+
+namespace BackendService.Models
+{
+    public class VerifyCodeModel
+    {
+        [Required]
+        public String UserId { get; set; }
+        [Required]
+        public String Code { get; set; }
+    }
+}
